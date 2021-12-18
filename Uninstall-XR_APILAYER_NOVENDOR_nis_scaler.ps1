a29e4e80055416179374826c0955bd0c7b744501
Matthieu Bucchianeri <mbucchia@gmail.com>
2021-12-18 12:37:26 -0800

    Remove the obsolete install scripts.

--- Uninstall-XR_APILAYER_NOVENDOR_nis_scaler.ps1
+++
@@ -1,6 +0,0 @@
-$JsonPath = Join-Path "$PSScriptRoot" "XR_APILAYER_NOVENDOR_nis_scaler.json"
-Start-Process -FilePath powershell.exe -Verb RunAs -Wait -ArgumentList @"
-	& {
-		Remove-ItemProperty -Path HKLM:\Software\Khronos\OpenXR\1\ApiLayers\Implicit -Name '$jsonPath' -Force | Out-Null
-	}
-"@
